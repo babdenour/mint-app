6fdf49799d7965220e09a62b56da9cacfba8cf60
abdenour <abdenourbensouna@gmail.com>
2022-03-01 13:57:18 -0500

    mint-ready

---
+++ bonaApp/mint/MintSC.ts
@@ -0,0 +1,71 @@
+import { BooleanType, BigUIntType, BinaryCodec, AddressValue, SmartContract, IDappProvider, IProvider, Address, Balance, ContractFunction, GasLimit, TransactionPayload, Account, Transaction } from "@elrondnetwork/erdjs";
+import { BigNumber } from "bignumber.js";
+import Providers from "../../src/providers/Providers";
+
+
+const Codec = new BinaryCodec();
+const gasLimite = 280000000;
+const ScAddress = process.env.ScAddress as string;
+const sca = "erd1qqqqqqqqqqqqqpgqy0tefkrf8zpz2mveldvrthutxyh9shl3de8s9q7826";
+
+class MintSC {
+  private readonly smartContractAddress: string = sca
+  private _provider: Providers;
+  constructor(provider: Providers) {
+    this._provider = provider;
+  }
+
+  async sendF(wallet: Address, amount: BigNumber) {
+    let account = new Account(wallet);
+    await account.sync(this._provider.proxy);
+
+    const payload = TransactionPayload.contractCall()
+      .setFunction(new ContractFunction("mint@01"))
+      .setArgs([])
+      .build();
+
+    const transaction = new Transaction({
+      sender: wallet,
+      receiver: new Address(this.smartContractAddress),
+      gasLimit: new GasLimit(gasLimite),
+      value: Balance.egld(amount),
+      data: payload,
+    });
+    transaction.setNonce(account.nonce);
+    return this._provider.sendAndWatch(transaction);
+  }
+
+  async didUserSend(wallet: Address) {
+    let contract = new SmartContract({ address: new Address(this.smartContractAddress) });
+    let result = await contract.runQuery(this._provider.proxy, {
+      func: new ContractFunction("didUserSend"),
+      args: [new AddressValue(wallet)]
+    });
+    let decoded = Codec.decodeTopLevel(new Buffer(result.outputUntyped()[0]), new BooleanType());
+    console.log("didUserSend", decoded.valueOf())
+    return decoded.valueOf();
+  }
+
+  async dateToReceived(wallet: Address) {
+    let contract = new SmartContract({ address: new Address(this.smartContractAddress) });
+    let result = await contract.runQuery(this._provider.proxy, {
+      func: new ContractFunction("getTimeToReceived"),
+      args: [new AddressValue(wallet)]
+    });
+    console.log("dateReceived", result.returnData)
+    return result;
+  }
+
+  async sendAmount() {
+    let contract = new SmartContract({ address: new Address(this.smartContractAddress) });
+    let result = await contract.runQuery(this._provider.proxy, {
+      func: new ContractFunction("getSendAmount"),
+      args: []
+    });
+    let decoded = Codec.decodeTopLevel(new Buffer(result.outputUntyped()[0]), new BigUIntType());
+    console.log("getSendAmount", decoded.valueOf())
+    return decoded.valueOf();
+  }
+}
+
+export default MintSC;
---
+++ bonaApp/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../home/Home.vue'
+import Mint from "../mint/Mint";
+import CustomQRCodeHandler from "./CustomQRCodeHandler";
+import { vueErdJsStore, VueErdjsConnect } from '../../src'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/authenticate',
+    name: 'VueErdjsConnect',
+    component: VueErdjsConnect,
+    props: { qrcodeHandler: new CustomQRCodeHandler(), token: "Bona Parks login" }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    beforeEnter (to) {
+      window.location = `/`
+    }
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: '/bonaparks/',
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.matched.some(record => record.meta.requiresAuth)) {
+    next();
+  } else if (!vueErdJsStore.logged) {
+    next({
+      path: '/authenticate',
+      query: { fromUrl: to.fullPath }
+    })
+  } else {
+    next();
+  }
+})
+
+export default router
